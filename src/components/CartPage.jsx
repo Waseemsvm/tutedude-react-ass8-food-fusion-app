7064dabc2844107707a5330504097105848ca8b8
Waseemsvm <waseemsvm14@gmail.com>
2025-08-10 21:22:42 +0530

    updated cart page

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -2,15 +2,18 @@ import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import CartPageStyles from "../styles/CartPage.module.css";
 import rupeeSymbol from "../assets/images/rupee.svg";
+import { useNavigate } from "react-router";
+import emptyCartImg from "../assets/images/empty-cart.svg";
 
 export default function CartPage() {
+  const navigate = useNavigate();
+
   const cartItems = useSelector((state) => state.cart.items);
   const subTotal = cartItems.reduce((acc, itm) => {
     acc += itm.quantity * itm.price;
     return acc;
   }, 0);
   const shipCharge = 250;
-  // const discount = (total + shipCharge) * 0.1;
   const discount = 10;
   const tax = 7;
   const total = subTotal - subTotal * 0.1 + subTotal * 0.1 * (tax / 100);
@@ -18,67 +21,98 @@ export default function CartPage() {
   return (
     <div className={CartPageStyles["cart-page"]}>
       <h3>MyCart</h3>
-      <table className={CartPageStyles.table}>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th className={CartPageStyles["tbl-hdr"]}>Qty</th>
-            <th className={CartPageStyles["tbl-hdr"]}>Subtotal</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItems.map((item) => (
-            <CartItem id={item.id} key={item.id} />
-          ))}
-          <tr>
-            <td>SubTotal</td>
-            <td></td>
-            <td>
-              <embed src={rupeeSymbol} className={CartPageStyles["rup-sym"]} />{" "}
-              {subTotal ?? 0}
-            </td>
-          </tr>
-          <tr>
-            <td>Shipping Charge</td>
-            <td></td>
-            <td>
-              <embed src={rupeeSymbol} className={CartPageStyles["rup-sym"]} />
-              {shipCharge}
-            </td>
-          </tr>
-          <tr>
-            <td>Discount</td>
-            <td></td>
-            <td>
-              <embed src={rupeeSymbol} className={CartPageStyles["rup-sym"]} />
-              {discount}%
-            </td>
-          </tr>
-          <tr>
-            <td>Tax</td>
-            <td></td>
-            <td>
-              <embed src={rupeeSymbol} className={CartPageStyles["rup-sym"]} />
-              {tax}%
-            </td>
-          </tr>
-          <tr>
-            <td>Total</td>
-            <td></td>
-            <td>
-              <embed src={rupeeSymbol} className={CartPageStyles["rup-sym"]} />
-              {grandTotal}
-            </td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td className={CartPageStyles["chk-btn-cont"]}>
-              <button>Checkout</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      {cartItems.length ? (
+        <table className={CartPageStyles.table}>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th className={CartPageStyles["tbl-hdr"]}>Qty</th>
+              <th className={CartPageStyles["tbl-hdr"]}>Subtotal</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item) => (
+              <CartItem id={item.id} key={item.id} />
+            ))}
+            <tr>
+              <td>SubTotal</td>
+              <td></td>
+              <td>
+                <embed
+                  src={rupeeSymbol}
+                  className={CartPageStyles["rup-sym"]}
+                />{" "}
+                {subTotal ?? 0}
+              </td>
+            </tr>
+            <tr>
+              <td>Shipping Charge</td>
+              <td></td>
+              <td>
+                <embed
+                  src={rupeeSymbol}
+                  className={CartPageStyles["rup-sym"]}
+                />
+                {shipCharge}
+              </td>
+            </tr>
+            <tr>
+              <td>Discount</td>
+              <td></td>
+              <td>
+                <embed
+                  src={rupeeSymbol}
+                  className={CartPageStyles["rup-sym"]}
+                />
+                {discount}%
+              </td>
+            </tr>
+            <tr>
+              <td>Tax</td>
+              <td></td>
+              <td>
+                <embed
+                  src={rupeeSymbol}
+                  className={CartPageStyles["rup-sym"]}
+                />
+                {tax}%
+              </td>
+            </tr>
+            <tr>
+              <td>Total</td>
+              <td></td>
+              <td>
+                <embed
+                  src={rupeeSymbol}
+                  className={CartPageStyles["rup-sym"]}
+                />
+                {grandTotal}
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td className={CartPageStyles["chk-btn-cont"]}>
+                <button>Checkout</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <div>
+          <div className={CartPageStyles["empty-cart-img-cont"]}>
+            <h2>Cart is Empty!!</h2>
+            <img src={emptyCartImg} alt="cart-empty" />
+            <button
+              onClick={(e) => {
+                navigate("/menu");
+              }}
+            >
+              Continue Shopping
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
